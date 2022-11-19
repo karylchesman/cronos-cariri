82b0bcd22d0713546079c4f73ef973ce5712f23c
karylchesman <karylbps@gmail.com>
2022-11-19 14:52:42 -0300

    Criando arquivo para armazenar opções de filtro de usuário

--- frontend/src/pages/Users/index.tsx
+++ frontend/src/pages/Users/index.tsx
@@ -23,6 +23,7 @@ import { SearchWithFilter } from '../../components/SearchWithFilter';
 import { useDataPagination } from '../../hooks/useDataPagination';
 import { UpdatePersonModal } from '../../components/UpdatePersonModal';
 import { IPerson } from '../../@types/persons';
+import userFilterOptions from './user-filter-options';
 
 interface IGetUsersReturnType {
     users: IUser[];
@@ -135,70 +136,7 @@ const Users = () => {
                                 }
                             }}
                             searchValue={searchPagination.search_params === undefined ? "" : searchPagination.search_params}
-                            filterOptions={
-                                [
-                                    {
-                                        alias: "Nome",
-                                        key: "name"
-                                    },
-                                    {
-                                        alias: "E-mail",
-                                        key: "email"
-                                    },
-                                    {
-                                        alias: "CPF",
-                                        key: "person:cpf"
-                                    },
-                                    {
-                                        alias: "Contato 1",
-                                        key: "person:phonenumber1"
-                                    },
-                                    {
-                                        alias: "Contato 2",
-                                        key: "person:phonenumber2"
-                                    },
-                                    {
-                                        alias: "Sexo",
-                                        key: "person:gender"
-                                    },
-                                    {
-                                        alias: "RG",
-                                        key: "person:rg"
-                                    },
-                                    {
-                                        alias: "Data de nascimento",
-                                        key: "person:bith_date"
-                                    },
-                                    {
-                                        alias: "Tipo sanguineo",
-                                        key: "person:blood_type"
-                                    },
-                                    {
-                                        alias: "Endereço - Rua",
-                                        key: "person:address_street"
-                                    },
-                                    {
-                                        alias: "Endereço - número",
-                                        key: "person:address_number"
-                                    },
-                                    {
-                                        alias: "Endereço - Bairro",
-                                        key: "person:address_district"
-                                    },
-                                    {
-                                        alias: "Endereço - Cidade",
-                                        key: "person:address_city"
-                                    },
-                                    {
-                                        alias: "Endereço - UF",
-                                        key: "person:address_uf"
-                                    },
-                                    {
-                                        alias: "Endereço - CEP",
-                                        key: "person:address_cep"
-                                    }
-                                ]
-                            }
+                            filterOptions={userFilterOptions}
                         />
                     </div>
 
---
+++ frontend/src/pages/Users/user-filter-options.ts
@@ -0,0 +1,62 @@
+export default [
+    {
+        alias: "Nome",
+        key: "name"
+    },
+    {
+        alias: "E-mail",
+        key: "email"
+    },
+    {
+        alias: "CPF",
+        key: "person:cpf"
+    },
+    {
+        alias: "Contato 1",
+        key: "person:phonenumber1"
+    },
+    {
+        alias: "Contato 2",
+        key: "person:phonenumber2"
+    },
+    {
+        alias: "Sexo",
+        key: "person:gender"
+    },
+    {
+        alias: "RG",
+        key: "person:rg"
+    },
+    {
+        alias: "Data de nascimento",
+        key: "person:bith_date"
+    },
+    {
+        alias: "Tipo sanguineo",
+        key: "person:blood_type"
+    },
+    {
+        alias: "Endereço - Rua",
+        key: "person:address_street"
+    },
+    {
+        alias: "Endereço - número",
+        key: "person:address_number"
+    },
+    {
+        alias: "Endereço - Bairro",
+        key: "person:address_district"
+    },
+    {
+        alias: "Endereço - Cidade",
+        key: "person:address_city"
+    },
+    {
+        alias: "Endereço - UF",
+        key: "person:address_uf"
+    },
+    {
+        alias: "Endereço - CEP",
+        key: "person:address_cep"
+    }
+]
